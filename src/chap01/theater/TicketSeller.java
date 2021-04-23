20fbeb5287cf8a76af1f1e5d80438afb61726e2c
Timo <developer.timo@gmail.com>
2021-04-23 12:01:19 +0900

    티켓 교환/구매가 관람객에 의해 행해지도록 코드 변경(캡슐화)

--- src/chap01/theater/TicketSeller.java
+++ src/chap01/theater/TicketSeller.java
@@ -12,17 +12,6 @@ public class TicketSeller {
 
     /* 티켓 교환/판매 */
     public void sellTo(Audience audience) {
-        // 초대장을 보유하여 티켓을 교환하는 경우
-        if (audience.getBag().hasInvitation()) {
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().setTicket(ticket);
-
-        // 티켓을 구매하는 경우
-        } else {
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketOffice.plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
     }
 }
